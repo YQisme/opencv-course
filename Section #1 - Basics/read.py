9018ad1457ff130e874fd1409805b830b70fc18d
YQisme <1398757912@qq.com>
2022-10-05 23:34:21 +0800

    第一小节

--- Section #1 - Basics/read.py
+++ Section #1 - Basics/read.py
@@ -1,28 +1,26 @@
-#pylint:disable=no-member
-
 import cv2 as cv
 
-img = cv.imread('../Resources/Photos/cats.jpg')
-cv.imshow('Cats', img)
+# img = cv.imread("Resources/Photos/cats.jpg")  # 返回像素矩阵
+# cv.imshow("Cats", img)
 
-cv.waitKey(0)
+# cv.waitKey(0)  # 设置等待时间，单位ms。0表示按键后继续执行
 
-# Reading Videos
-capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
 
+# 读取视频，参数是0表示读取摄像头
+# capture = cv.VideoCapture(0)
+capture = cv.VideoCapture("Resources/Videos/dog.mp4")
+
+# 在读取视频时，使用while循环，每次读取一帧,读取的帧保存在frame中
+# 读取成功，返回True，否则返回False，视频播放完就结束
+# 或者在读取播放时按d键退出
 while True:
     isTrue, frame = capture.read()
-    
-    # if cv.waitKey(20) & 0xFF==ord('d'):
-    # This is the preferred way - if `isTrue` is false (the frame could 
-    # not be read, or we're at the end of the video), we immediately
-    # break from the loop. 
-    if isTrue:    
-        cv.imshow('Video', frame)
-        if cv.waitKey(20) & 0xFF==ord('d'):
-            break            
+    if isTrue:
+        cv.imshow("Video", frame)
+        if cv.waitKey(20) & 0xFF == ord("d"):
+            break
     else:
         break
-
+# 释放捕获设备并销毁所有窗口
 capture.release()
 cv.destroyAllWindows()
